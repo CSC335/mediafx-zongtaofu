72c064edb23adb3029584ce93f5129129dd6db8a
zongtaofu <35672848+zongtaofu@users.noreply.github.com>
2023-03-01 22:19:43 -0700

    Made small changes to PlayOneMP3.java

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -19,48 +19,50 @@ import javafx.stage.Stage;
 
 public class PlayAnMP3 extends Application {
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-  private int songsPlayed = 0;
+	private int songsPlayed = 0;
 
-  @Override
-  public void start(Stage stage) throws Exception {
-    BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
-    pane.setCenter( new Label(path));
-    playASong(path);
-    // Put the pane in a sized Scene and show the GUI
-    Scene scene = new Scene(pane, 255, 85); // 255 pixels wide, 85 pixels tall
-    stage.setScene(scene);
-    // Don't forget to show the running app:
-    stage.show();
-  }
+	@Override
+	public void start(Stage stage) throws Exception {
+		BorderPane pane = new BorderPane();
+		// i, Zongtao Fu, changed the somg to be played before pushing to github
+		String path = "songfiles/DanseMacabreViolinHook.mp3";
+		
+		playASong(path);
+		// and swapped 2 lines and adding 1 empty line
+		pane.setCenter(new Label(path));
+		// Put the pane in a sized Scene and show the GUI
+		Scene scene = new Scene(pane, 255, 85); // 255 pixels wide, 85 pixels tall
+		stage.setScene(scene);
+		// Don't forget to show the running app:
+		stage.show();
+	}
 
-  
-  private void playASong(String path) {
-   
-    // Need a File and URI object so the path works on all OSs
-    File file = new File(path);
-    URI uri = file.toURI();
-    System.out.println(uri);
-    // Play one mp3 and and have code run when the song ends
-    Media media = new Media(uri.toString());
-    MediaPlayer mediaPlayer = new MediaPlayer(media);
-    mediaPlayer.play();
-      
-    mediaPlayer.setOnEndOfMedia(new Waiter());
-    System.out.println("You may need to shut this App down");
- 
-    }
-  
-  private class Waiter implements Runnable {
-    @Override
-    public void run() {
-      songsPlayed++;
-      System.out.println("Song ended, play song #" + songsPlayed);
-      Platform.exit();
-    }
-  }
+	private void playASong(String path) {
+
+		// Need a File and URI object so the path works on all OSs
+		File file = new File(path);
+		URI uri = file.toURI();
+		System.out.println(uri);
+		// Play one mp3 and and have code run when the song ends
+		Media media = new Media(uri.toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.play();
+
+		mediaPlayer.setOnEndOfMedia(new Waiter());
+		System.out.println("You may need to shut this App down");
+
+	}
+
+	private class Waiter implements Runnable {
+		@Override
+		public void run() {
+			songsPlayed++;
+			System.out.println("Song ended, play song #" + songsPlayed);
+			Platform.exit();
+		}
+	}
 }
